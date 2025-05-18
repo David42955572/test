de6d85a9320f7da9d72813a6ed89be11490af8a4
David13285 <ssea4295@gmail.com>
2025-05-19 00:51:02 +0800

    Create protocol.h

---
+++ protocol.h
@@ -0,0 +1,18 @@
+#ifndef PROTOCOL_H
+#define PROTOCOL_H
+
+#include <stdint.h>
+
+#define MAX_DATA_SIZE 1024
+
+typedef struct {
+    uint8_t operation;
+    uint8_t status;
+    uint16_t length;
+} ProtocolHeader;
+
+int pack_message(uint8_t operation, uint8_t status, uint8_t *data, uint16_t length, uint8_t *buffer);
+int parse_header(const uint8_t *buffer, ProtocolHeader *header);
+int parse_data(const uint8_t *buffer, uint16_t length, uint8_t *data);
+
+#endif // PROTOCOL_H
