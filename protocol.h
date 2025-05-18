b32011a07ed3b414bd5c6bd4ce8ecbcaab40a6e9
David13285 <ssea4295@gmail.com>
2025-05-19 00:52:30 +0800

    Update protocol.h

--- protocol.h
+++ protocol.h
@@ -11,6 +11,7 @@ typedef struct {
     uint16_t length;
 } ProtocolHeader;
 
+void pack_header(uint8_t operation, uint8_t status, uint32_t length, uint8_t *buffer);
 int pack_message(uint8_t operation, uint8_t status, uint8_t *data, uint16_t length, uint8_t *buffer);
 int parse_header(const uint8_t *buffer, ProtocolHeader *header);
 int parse_data(const uint8_t *buffer, uint16_t length, uint8_t *data);
