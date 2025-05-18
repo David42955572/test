d0525e615ea9be0e5bb833fdd8ed55b338bfbe38
David13285 <ssea4295@gmail.com>
2025-05-19 04:12:02 +0800

    Update protocol.c

--- protocol.c
+++ protocol.c
@@ -6,33 +6,40 @@
 
 #define MAX_DATA_SIZE 1024
 
-
 // 封裝完整協議包
-int pack_message(uint8_t operation, uint8_t status, const uint8_t *data, uint32_t data_length, uint8_t *buffer) {
+int pack_message(uint8_t operation, uint8_t status, const char *username, const uint8_t *data, uint32_t data_length, uint8_t *buffer) {
+    uint8_t username_len = strlen(username);
     if (data_length > MAX_DATA_SIZE) return -1;
 
     // 封裝頭部
     buffer[0] = operation;
     buffer[1] = status;
+    buffer[2] = username_len;
+    memcpy(buffer + 3, username, username_len);
+
     uint32_t net_length = htonl(data_length);
-    memcpy(buffer + 2, &net_length, sizeof(uint32_t));
+    memcpy(buffer + 3 + username_len, &net_length, sizeof(uint32_t));
 
     // 封裝數據區
-    memcpy(buffer + 6, data, data_length);
+    memcpy(buffer + 3 + username_len + 4, data, data_length);
 
-    return 6 + data_length;  // 返回總長度
+    return 3 + username_len + 4 + data_length;
 }
 
 // 解析協議頭部
 int parse_header(const uint8_t *buffer, ProtocolHeader *header) {
     header->operation = buffer[0];
     header->status = buffer[1];
+    header->username_len = buffer[2];
+
+    // 提取使用者名稱
+    memcpy(header->username, buffer + 3, header->username_len);
+    header->username[header->username_len] = '\0';
 
     uint32_t net_length;
-    memcpy(&net_length, buffer + 2, sizeof(uint32_t));
-
+    memcpy(&net_length, buffer + 3 + header->username_len, sizeof(uint32_t));
     header->length = ntohl(net_length);
-    
+
     return 0;
 }
 
