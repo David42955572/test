2ad38092b0dc4b6829cd9f0e905a0733261ff907
David13285 <ssea4295@gmail.com>
2025-05-19 13:58:11 +0800

    Update protocol.c

--- protocol.c
+++ protocol.c
@@ -7,7 +7,7 @@
 #define MAX_DATA_SIZE 1024
 
 // 封裝完整協議包
-int pack_message(uint8_t operation, uint8_t status, const char *username, const uint8_t *data, uint32_t data_length, uint8_t *buffer) {
+int pack_message(uint8_t operation, uint8_t status, const char *username, uint32_t sequence, const uint8_t *data, uint32_t data_length, uint8_t *buffer) {
     uint8_t username_len = strlen(username);
     if (data_length > MAX_DATA_SIZE) return -1;
 
@@ -17,13 +17,15 @@ int pack_message(uint8_t operation, uint8_t status, const char *username, const 
     buffer[2] = username_len;
     memcpy(buffer + 3, username, username_len);
 
-    uint32_t net_length = htonl(data_length);
-    memcpy(buffer + 3 + username_len, &net_length, sizeof(uint32_t));
+    uint32_t net_sequence = htonl(sequence);
+    memcpy(buffer + 3 + username_len, &net_sequence, sizeof(uint32_t));
 
     // 封裝數據區
     memcpy(buffer + 3 + username_len + 4, data, data_length);
 
-    return 3 + username_len + 4 + data_length;
+    buffer[3 + username_len + 4 + data_length] = '\0';
+
+    return 3 + username_len + 4 + data_length + 1;
 }
 
 // 解析協議頭部
@@ -36,16 +38,22 @@ int parse_header(const uint8_t *buffer, ProtocolHeader *header) {
     memcpy(header->username, buffer + 3, header->username_len);
     header->username[header->username_len] = '\0';
 
-    uint32_t net_length;
-    memcpy(&net_length, buffer + 3 + header->username_len, sizeof(uint32_t));
-    header->length = ntohl(net_length);
+    // 解析 sequence
+    uint32_t net_sequence;
+    memcpy(&net_sequence, buffer + 3 + header->username_len, sizeof(uint32_t));
+    header->sequence = ntohl(net_sequence);
+
+    // 計算剩餘數據區長度
+    uint32_t data_offset = 3 + header->username_len + 4;
+    header->length = strlen((char *)(buffer + data_offset));
 
     return 0;
 }
 
 // 解析數據區
 int parse_data(const uint8_t *buffer, uint32_t length, uint8_t *output) {
-    memcpy(output, buffer, length);
-    output[length] = '\0';  // 確保結尾是空字元
+    
+    memcpy(output, buffer, length+1);  // 確保結尾是空字元
+    
     return 0;
 }
