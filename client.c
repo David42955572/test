d3c0cc7084aaa7247451c79af64bafa3048346d6
David13285 <ssea4295@gmail.com>
2025-05-20 05:26:55 +0800

    Update client.c

--- client.c
+++ client.c
@@ -62,7 +62,7 @@ int client_send(int sockfd, uint8_t operation, uint8_t status, const char *usern
 int client_receive(int sockfd, const char *username, uint32_t *sequence, uint8_t data[MAX_DATA_SIZE]) {
     uint8_t buffer[MAX_DATA_SIZE];
     int received = recv(sockfd, buffer, MAX_DATA_SIZE, 0);
-    if (received < 7) {
+    if (received < 11) {
         fprintf(stderr, "收到的數據不足協議頭部長度\n");
         return -1;
     }
@@ -79,7 +79,7 @@ int client_receive(int sockfd, const char *username, uint32_t *sequence, uint8_t
     }
 
     // 解析數據
-    parse_data(buffer + 3 + header.username_len + 4, header.length, data);
+    parse_data(buffer + 3 + header.username_len + 8, header.length, data);
 
     // 取得 sequence
     uint32_t seq_num = header.sequence;
@@ -175,7 +175,7 @@ int client_send_file_content(int sockfd, const char *username, const char *filep
     size_t read_bytes;
     uint32_t sequence_number = 1;
 
-    while ((read_bytes = fread(buffer, 1, MAX_DATA_SIZE - 8 - strlen(username), fp)) > 0) {
+    while ((read_bytes = fread(buffer, 1, MAX_DATA_SIZE - 11 - strlen(username), fp)) > 0) {
 
         int sent = client_send(sockfd, 3, 0, username, &sequence_number, (uint8_t *)buffer, read_bytes);
         if (sent < 0) {
