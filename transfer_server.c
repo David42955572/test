4c540247e4dd1fc2de4f8a5695ce104f6bf82789
David13285 <ssea4295@gmail.com>
2025-05-20 07:30:56 +0800

    Update transfer_server.c

--- transfer_server.c
+++ transfer_server.c
@@ -87,15 +87,14 @@ void transfer_data(int src_socket, int dest_socket, int face) {
 
         // 嘗試解析完整封包
         while (1) {
-            printf ("%d\n",buffer_len);
+            
             if (buffer_len < 11) break;
-
-            printf ("%d\n",buffer_len);
+           
             uint8_t username_len = recv_buffer[2];
             int header_len = 3 + username_len + 8;
             if (buffer_len < header_len) break;
 
-            printf ("%d\n",buffer_len);
+            
             uint32_t data_len;
             memcpy(&data_len, recv_buffer + 3 + username_len + 4, 4);
             data_len = ntohl(data_len);
@@ -103,7 +102,6 @@ void transfer_data(int src_socket, int dest_socket, int face) {
             int total_packet_len = header_len + data_len;
             if (buffer_len < total_packet_len) break;
 
-            printf ("%d\n",buffer_len);
             ProtocolHeader header;
             if (parse_header(recv_buffer, &header) == -1) {
                 fprintf(stderr, "協議解析失敗 3\n");
@@ -111,7 +109,6 @@ void transfer_data(int src_socket, int dest_socket, int face) {
                 buffer_len = 0;
                 break;
             }
-            printf ("%d\n",buffer_len);
             
             uint8_t data[MAX_DATA_SIZE + 1];
             parse_data(recv_buffer + 3 + header.username_len + 8, header.length, data);
